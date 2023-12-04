0318bfb7402ad98da1ee0f29ba307ff155d6f70b
Melvin Snijders (student) <s1154771@student.windesheim.nl>
2023-12-04 12:53:14 +0100

    feat: :sparkles: Solved Day 4 part 2

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -9,7 +9,7 @@ const parseInput = (rawInput: string): Game[] => {
       .split(/\s+\|\s+/);
     const winningNumbers = allNumbers[0].split(/\s+/).map((n) => +n);
     const numbers = allNumbers[1].split(/\s+/).map((n) => +n);
-    return { id: +gameId, winningNumbers, numbers };
+    return { id: +gameId, winningNumbers, numbers, instances: 1 };
   });
 };
 
@@ -17,6 +17,7 @@ type Game = {
   id: number;
   numbers: number[];
   winningNumbers: number[];
+  instances: number;
 };
 
 const part1 = (rawInput: string) => {
@@ -33,7 +34,18 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  input.forEach((game) => {
+    const winningCount = game.winningNumbers.filter((number) => game.numbers.includes(number)).length;
+    for (let inst = 0; inst < game.instances; inst++) {
+      for (let i = 0; i < winningCount; i++) {
+        input[game.id + i].instances++;
+      }
+    }
+  });
+
+  const totalInstances = input.map((game) => game.instances).reduce((a, b) => a + b);
+
+  return totalInstances;
 };
 
 run({
@@ -55,10 +67,17 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+`,
+        expected: 30,
+      },
     ],
     solution: part2,
   },
